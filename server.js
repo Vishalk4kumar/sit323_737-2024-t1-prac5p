6dd5a0071d2c2b81ddae3a5ac9d7078ab86babfc
Vishalk4kumar <s222342946@deakin.edu.au>
2024-03-18 01:54:44 +1100

    Commit
    Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server on port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
